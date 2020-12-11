c18d3535d3a0739479fabae1947a767e10c244cf
Max Beneke <max.beneke@gmail.com>
2020-12-11 15:58:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
         if !final_hash[first_name][outer_key]
           final_hash[first_name][outer_key] = []
         end
-        final_hash[first_name][outer_key].push(inner_key)
+        final_hash[first_name][outer_key].push(inner_key.to_s)
       end
     end
     binding.pry
