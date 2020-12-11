11ad05ab19940bd1195cbfbbc439e7541702117c
Max Beneke <max.beneke@gmail.com>
2020-12-11 15:54:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,18 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   
-  data.each_with_object({}) { |(outer_key, value), final_hash|
+  data.each_with_object({}) do |(outer_key, value), final_hash|
     
-    value.each { |inner_key, pigeon_names|
+    value.each do |inner_key, pigeon_names|
       
-      pigeon_names.each { |first_name|
+      pigeon_names.each do |first_name|
         if !final_hash[first_name]
           final_hash[first_name] = {}
         end
         if !final_hash[first_name][]
-      }
+      end
       binding.pry
      puts "hlelo"
-    }
-  }  #end of initial iteration, no access to final_hash
+    end
+  end  #end of initial iteration, no access to final_hash
 end
